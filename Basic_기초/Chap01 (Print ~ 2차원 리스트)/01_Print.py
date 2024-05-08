0b658d1fdb89df79ce84518efc8a304a72eb510e
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-08 21:00:15 +0900

    Create 01_Print.py

---
+++ Basic_기초/Chap01 (Print ~ 2차원 리스트)/01_Print.py
@@ -0,0 +1,31 @@
+# print : 파이썬에서 출력을 담당하는 기능
+# 문자열을 출력해보자
+print('my first python code!') # my first python code! 출력
+print('Hello World!') # Hello World! 출력
+
+# 정수 출력해보기 문자열과 다르게 따옴표로 감싸줄 필요가 없다.
+print(3) # 3 출력
+print(5) # 5 출력
+
+# Print 에서 사칙연산 해보기
+print(1 + 2) # 3 출력
+print(5 - 2) # 2 출력
+
+print(10 - 5) # 5 출력
+print(6 - 3) # 3 출력
+
+# ' * ' 는 곱하기를 의미하는 기호이다.
+print(2 * 3) # 6 출력
+print(5 * 5) # 25 출력
+
+# ' / ' 는 나누기를 의미하는 기호이다.
+print(8 / 2) # 4 출력
+print(100 / 10) # 10 출력
+
+# ' % ' 는 나눗값의 나머지를 계산할때 사용된다.
+print(4 % 2) # 0 출력
+print(10 % 3) # 1 출력
+
+# 논리형 출력해보기 (True, False)
+print(2 == 2) # True 출력 ' == ' 기호는 같은지를 판단한다 예를 들어 A == A 이면 A가 A와 같은지를 물어보는것이다 A는 A이기 때문에 True가 출력된다.
+print(5 == 3) # False 출력
