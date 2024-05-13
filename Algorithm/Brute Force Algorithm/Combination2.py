1b77cbb71853f8a3efa6647af409464861355265
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-14 08:35:46 +0900

    Create Combination2.py

---
+++ Algorithm/Brute Force Algorithm/Combination2.py
@@ -0,0 +1,8 @@
+# 조합을 사용하기 위해서는 combinations라는 내장 함수를 사용합니다. 이 함수는 itertools라는 파이썬 내장 패키지에 있습니다.
+# 사용법
+# combinations(리스트, 조합수)
+  
+from itertools import combinations
+
+arr = ['A', 'B', 'C']
+print(list(combinations(arr, 2))) # [('A', 'B'), ('A', 'C'), ('B', 'C')]
