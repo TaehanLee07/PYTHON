34d31a51ed6dced6c6d372dcba023bfc9e2c7928
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-14 09:44:37 +0900

    Update 02_이터레이터.py

--- Basic_기초/Chap03 (예외 철처리~ 모듈 과 패키지 )/02_이터레이터.py
+++ Basic_기초/Chap03 (예외 철처리~ 모듈 과 패키지 )/02_이터레이터.py
@@ -31,3 +31,22 @@ class 이터레이터이름:
  
     def __next__(self):
         코드
+
+class Counter:
+    def __init__(self, stop):
+        self.cur = 0    # 현재 숫자 유지, 0부터 지정된 숫자 직전까지 반복
+        self.stop = stop    # 반복을 끝낼 숫자
+ 
+    def __iter__(self):
+        return self        
+ 
+    def __next__(self):
+        if self.cur < self.stop:    # 현재 숫자가 반복을 끝낼 숫자보다 작을 때
+            r = self.cur           
+            self.cur += 1           
+            return r                    
+        else:                           # 현재 숫자가 반복을 끝낼 숫자보다 크거나 같을 때
+            raise StopIteration         # 예외 발생
+ 
+for i in Counter(3):
+    print(i, end=' ') # 0 1 2 출력
